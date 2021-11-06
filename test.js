501ec18eda1c4dad37300c7be4f7217a281b07f9
ahmetov <ahmetov@litres.ru>
2021-11-06 16:37:22 +0300

    updates

--- test.js
+++ test.js
@@ -4,12 +4,14 @@ var h1 = document.querySelector('.text')
 button.addEventListener('click', handleLinkClick)
 
 function handleLinkClick (event) {
-if (h1.style.display === 'none') {
-h1.style.display = 'flex'
-h1.innerHTML = 'Текст, который выводится'
-} else {
-    var div = document.createElement('div');
-    div.className = "text";
-    div.innerHTML = "Текст, который выводится";
-    h1.after(div);
-}}
+    const appendedDiv = `
+        <div class = "text">Текст, который выводится</div>
+    `;
+
+    if (h1.style.display === 'none') {
+        h1.style.display = 'flex'
+        h1.innerHTML = 'Текст, который выводится'
+    } else {
+        h1.insertAdjacentHTML("afterend", appendedDiv);
+    }
+}
